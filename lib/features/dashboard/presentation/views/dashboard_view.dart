8751c7097d2e15057ea33f3572f0226bcbb841cc
mohamedhassan <mohamed7github@gmail.com>
2024-07-20 06:52:45 +0300

    create main view

---
+++ lib/features/dashboard/presentation/views/dashboard_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Dashboardview extends StatelessWidget {
+  const Dashboardview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/main/presentation/views/main_view.dart
+++ lib/features/main/presentation/views/main_view.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app_admin_panel/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:ecommerce_app_admin_panel/features/main/presentation/views/widgets/side_menu.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatelessWidget {
@@ -5,6 +7,19 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+          child: Row(
+        children: [
+          Expanded(
+            child: SideMenu(),
+          ),
+          Expanded(
+            flex: 5,
+            child: Dashboardview(),
+          ),
+        ],
+      )),
+    );
   }
 }
