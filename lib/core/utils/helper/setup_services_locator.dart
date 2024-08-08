87645f3de914727752cf9ee8c101cd729e004d7f
mohamedhassan <mohamed7github@gmail.com>
2024-08-08 08:25:57 +0300

    refactor code

--- lib/core/utils/helper/setup_services_locator.dart
+++ lib/core/utils/helper/setup_services_locator.dart
@@ -1,13 +1,27 @@
 import 'package:dio/dio.dart';
 import 'package:ecommerce_app_admin_panel/core/utils/services/api_services.dart';
+import 'package:ecommerce_app_admin_panel/features/category/data/data_sources/category_remote_data_source.dart';
+import 'package:ecommerce_app_admin_panel/features/category/data/repos/category_repo_impl.dart';
+import 'package:ecommerce_app_admin_panel/features/category/domain/use_cases/category_use_case.dart';
 import 'package:get_it/get_it.dart';
 
-final gitIt = GetIt.instance;
+final getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  gitIt.registerSingleton<ApiService>(
+  // Register API service
+  getIt.registerSingleton<ApiService>(
     ApiService(
       Dio(),
+    ),
+  );
+  // Register category service
+  getIt.registerSingleton<CategoryUseCaseImp>(
+    CategoryUseCaseImp(
+      CategoryRepoImpl(
+        CategoryRemoteDataSourceImpl(
+          getIt.get<ApiService>(),
+        ),
+      ),
     ),
   );
 }
--- lib/features/category/presentatation/views/category_view.dart
+++ lib/features/category/presentatation/views/category_view.dart
@@ -1,18 +1,14 @@
 import 'package:ecommerce_app_admin_panel/core/utils/constants/constants.dart';
 import 'package:ecommerce_app_admin_panel/core/utils/functions/show_add_category_form.dart';
-import 'package:ecommerce_app_admin_panel/core/utils/services/api_services.dart';
+import 'package:ecommerce_app_admin_panel/core/utils/helper/setup_services_locator.dart';
 import 'package:ecommerce_app_admin_panel/core/widgets/custom_app_bar.dart';
 import 'package:ecommerce_app_admin_panel/core/widgets/main_view_header.dart';
-import 'package:ecommerce_app_admin_panel/features/category/data/data_sources/category_remote_data_source.dart';
-import 'package:ecommerce_app_admin_panel/features/category/data/repos/category_repo_impl.dart';
 import 'package:ecommerce_app_admin_panel/features/category/domain/use_cases/category_use_case.dart';
 import 'package:ecommerce_app_admin_panel/features/category/presentatation/manager/cubit/category_cubit.dart';
 import 'package:ecommerce_app_admin_panel/features/category/presentatation/views/widgets/category_list_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
-final getIt = GetIt.instance;
 
 class CategoryView extends StatelessWidget {
   const CategoryView({super.key});
@@ -21,13 +17,7 @@ class CategoryView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CategoryCubit(
-        CategoryUseCaseImp(
-          CategoryRepoImpl(
-            CategoryRemoteDataSourceImpl(
-              getIt.get<ApiService>(),
-            ),
-          ),
-        ),
+        getIt.get<CategoryUseCaseImp>(),
       ),
       child: SafeArea(
         child: SingleChildScrollView(
